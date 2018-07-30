5aa8c8a4d9a9f5c0be18238832e1173a556e30c4
Nobuhiro Takahashi <nt@feb19.jp>
2018-07-31 08:41:43 +0900

    update

--- MenuBarApp-macOS/AppDelegate.swift
+++ MenuBarApp-macOS/AppDelegate.swift
@@ -11,16 +11,36 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(AppDelegate.update), userInfo: nil, repeats: true)
+        //Show statusMenu
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @objc func update() {
+        // Format the date
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "h:mm:ss"
+        
+        // Convert date into a string
+        let now = dateformatter.string(from: NSDate() as Date)
+        
+        // Replace the title of the item in the status bar
+        statusItem.title = "⏳"+now
+    }
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
 }
 
--- MenuBarApp-macOS/ViewController.swift
+++ MenuBarApp-macOS/ViewController.swift
@@ -9,10 +9,9 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
