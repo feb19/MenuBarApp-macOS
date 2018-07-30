5e06ff25e39a8b15bb30dfcf77c40910a0097a28
Nobuhiro Takahashi <nt@feb19.jp>
2018-07-31 08:23:06 +0900

    Initial Commit

---
+++ MenuBarApp-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuBarApp-macOS
+//
+//  Created by Nobuhiro Takahashi on 2018/07/31.
+//  Copyright © 2018年 Nobuhiro Takahashi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenuBarApp-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBarApp-macOS
+//
+//  Created by Nobuhiro Takahashi on 2018/07/31.
+//  Copyright © 2018年 Nobuhiro Takahashi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
